39e4b8d51d0929e4306b3bf652e6c41f0921aadf
Ron Juarez <ronstewartjuarez@gmail.com>
2020-03-18 18:03:11 +0000

    return vs console.log

--- hello.js
+++ hello.js
@@ -12,3 +12,20 @@ const sayHello  = function (name) {
 sayHello("Caliban");
 sayHello("Miranda");
 sayHello("Ferdinand");
+
+
+/*For the rest of this exercise, we will be using functions that return a value. This means that we will have 
+to console.log our values outside of the function.*/
+
+const sayHelloToConsole  = function (name) {
+  console.log("Hello, " + name);
+}
+sayHelloToConsole('John'); 
+
+
+const returnSayHello  = function (name) {
+  return "Hello, " + name;
+}
+const greeting = returnSayHello('John');
+
+console.log(greeting);
