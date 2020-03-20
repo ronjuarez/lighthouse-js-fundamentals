2d5b3226d822d883b73c86849f04b92df9c8863e
Ron Juarez <ronstewartjuarez@gmail.com>
2020-03-20 11:48:42 +0000

    banana bread iterating over arrays assignment

---
+++ ingredients.js
@@ -0,0 +1,22 @@
+const ingredients = ["eggs", "milk", "flour", "sugar", "baking soda", "baking powder", "chocolate chips", "bananas"];
+
+// Write a while loop that prints out the contents of ingredients:
+  
+i = 0;
+
+while (i <= 7){
+  
+  console.log(ingredients[i])
+  i++
+}
+
+// Write a for loop that prints out the contents of ingredients:
+for (let i = 0; i <= 7; i++){
+  console.log(ingredients[i])
+}
+
+// Write any loop (while or for) that prints out the contents of ingredients backward
+
+for (let i = 7; i >= 0; i--){
+  console.log(ingredients[i])
+}
