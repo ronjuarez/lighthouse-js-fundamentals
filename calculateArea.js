c5f05c9f4cecf47c4f2efbbcdf4856f5aee8b256
Ron Juarez <ronstewartjuarez@gmail.com>
2020-03-20 10:43:16 +0000

    function that performs the formula for calculating the area of a shape

---
+++ calculateArea.js
@@ -0,0 +1,34 @@
+let calculateRectangleArea = function (length, width){
+  let rectangleArea = length * width;
+  if (rectangleArea > 0){
+  return rectangleArea;
+  }
+}
+
+
+let calculateTriangleArea = function (base, height){
+  let triangleArea = base * height / 2;
+  if (triangleArea > 0){
+  return triangleArea;
+  }
+}
+
+
+let calculateCircleArea = function (radius){
+  let circleArea = Math.PI * radius ** 2;
+  if (radius > 0){
+  return circleArea;
+  }
+}
+
+console.log(calculateRectangleArea(10, 5));     // should print 50
+console.log(calculateRectangleArea(1.5, 2.5));  // should print 3.75
+console.log(calculateRectangleArea(10, -5));    // should print undefined
+
+console.log(calculateTriangleArea(10, 5)); // should print 25
+console.log(calculateTriangleArea(3, 2.5)); // should print 3.75
+console.log(calculateTriangleArea(10, -5)); // should print undefined
+
+console.log(calculateCircleArea(10)); // should print 314.159...
+console.log(calculateCircleArea(3.5)); // should print 38.484...
+console.log(calculateCircleArea(-1)); // should print undefined
