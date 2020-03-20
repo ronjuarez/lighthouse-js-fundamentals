9c773ffc5596104d76e688ab287834cfbc0d514d
Ron Juarez <ronstewartjuarez@gmail.com>
2020-03-20 21:15:53 +0000

    this loop determines the last index of a number

---
+++ lastIndexOf.js
@@ -0,0 +1,25 @@
+
+let lastIndexOf = function(arrayOfNum, num) {
+  let match = false;
+  let result;
+  
+  for (let i = arrayOfNum.length - 1; i >= 0; i--){
+    if (arrayOfNum[i] === num) {
+      result = i;
+      match = true;
+      break;
+    }
+  }    
+  if (result >= 0 && match) {
+    return result;
+  } else {
+    return -1;
+  }
+};
+
+
+console.log(lastIndexOf([ 0, 1, 4, 1, 2 ], 1), "=?", 3);
+console.log(lastIndexOf([ 0, 1, 4, 1, 2 ], 2), "=?", 4);
+console.log(lastIndexOf([ 0, 1, 4, 1, 2 ], 3), "=?", -1);
+console.log(lastIndexOf([ 5, 5, 5 ], 5), "=?", 2);
+console.log(lastIndexOf([], 3), "=?", -1);
